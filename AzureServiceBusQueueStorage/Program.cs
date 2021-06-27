2324a91e82f970decfe1a0ff1c1fdf6b610000c4
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2021-06-27 16:51:50 +0545

    service bus demo code uplaoded

---
+++ AzureServiceBusQueueStorage/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+
+namespace AzureServiceBusQueueStorage
+{
+    internal class Program
+    {
+        private const string connectionString = "<connectionstring>";
+        private const string queueName = "<queue name>";
+        private const int maxMessage = 10;
+
+        private static void Main(string[] args)
+        {
+            var res = "y";
+            do
+            {
+                QueueClient client = new QueueClient(connectionString, queueName);
+                client.CreateIfNotExists();
+                for (int i = 0; i < maxMessage; i++)
+                {
+                    client.SendMessage($"Message from Batch2 {DateTime.Now}");
+                }
+
+                QueueProperties prop = client.GetProperties();
+                int? cahcedMessage = prop.ApproximateMessagesCount;
+
+                Console.WriteLine("Reading message from queue without removing them");
+
+                PeekedMessage[] peeked = client.PeekMessages((int)cahcedMessage);
+
+                Console.ReadLine();
+                foreach (PeekedMessage item in peeked)
+                {
+                    Console.WriteLine($"Message read from the queue {System.Text.Encoding.ASCII.GetString(item.Body)}");
+
+                    prop = client.GetProperties();
+                    int? queueLength = prop.ApproximateMessagesCount;
+                    Console.WriteLine($"Current Length of the queue is {queueLength}");
+                }
+
+                Console.ReadLine();
+
+                Console.WriteLine("Reading message fromt he queue");
+                QueueMessage[] messages = client.ReceiveMessages((int)cahcedMessage);
+                foreach (QueueMessage item in messages)
+                {
+                    Console.WriteLine($"Message read from the queue {System.Text.Encoding.ASCII.GetString(item.Body)}");
+                    client.DeleteMessage(item.MessageId, item.PopReceipt);
+
+                    prop = client.GetProperties();
+                    int? queueLength = prop.ApproximateMessagesCount;
+                    Console.WriteLine($"Current Length of the queue is {queueLength}");
+                }
+
+                Console.WriteLine("Want to try more? (y/n)");
+                res = Console.ReadLine();
+            } while (res.ToUpper() == "Y");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp.ServiceBus/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+
+namespace ConsoleApp.ServiceBus
+{
+    internal class Program
+    {
+        private const string ServiceBusConnectionString = "<connection string>";
+        private const string TopicName = "<topic name>";
+        private const int numberOfMessagesToSend = 100;
+
+        private static ITopicClient topicClient;
+
+        private static void Main(string[] args)
+        {
+            topicClient = new TopicClient(ServiceBusConnectionString, TopicName);
+
+            Console.WriteLine("Press ENTER key to exit after sending all the messages.");
+
+            // Send messages.
+            try
+            {
+                for (var i = 0; i < numberOfMessagesToSend; i++)
+                {
+                    // Create a new message to send to the topic.
+                    string messageBody = $"Message {i} {DateTime.Now}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the topic.
+                    topicClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+            Console.ReadKey();
+
+            topicClient.CloseAsync();
+            Console.ReadLine();
+        }
+    }
+}
